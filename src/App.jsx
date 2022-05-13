3091cbbce96bb25cbb2e7254201a7398c3c146ca
hauyne109 <88616807+hauyne109@users.noreply.github.com>
2022-05-14 00:41:05 +0900

    state追加

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
+import ColorfulMessage from "./components/ColorfulMessage";
 
 const App = () => {
-  const onClickButton = () => alert();
-  const contentStyle = {
-    color: "blue",
-    fontSize: "18px"
+  const onClickCountUp = () => {
+    setNum(num + 1);
   };
+  const [num, setNum] = useState(0);
   return (
     <>
       <h1 style={{ color: "red" }}>こんにちは！</h1>
-      <p style={contentStyle}>お元気ですか？</p>
-      <button onClick={onClickButton}>ボタン</button>
+      <ColorfulMessage color="blue">お元気ですか？</ColorfulMessage>
+      <ColorfulMessage color="pink">元気です!</ColorfulMessage>
+      <button onClick={onClickCountUp}>カウントアップ</button>
+      <p>{num}</p>
     </>
   );
 };
